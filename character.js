76102ab28637f2fd16aff0d5b9dc1e3883d5c004
DariusGuillaume <dariusguillaume2@gmail.com>
2024-03-26 06:35:13 -0400

    added jumping

--- character.js
+++ character.js
@@ -1,3 +1,5 @@
+import { getCustomProperty, incrementCustomProperty, setCustomProperty } from "./customProperty.js"
+
 const characterElement = document.querySelector("[data-character]")
 const JUMPSPEED = .45
 const GRAVITY = .015
@@ -8,21 +10,26 @@ const FRAME_TIME = 100
 let isJumping 
 let characterFrame
 let currentFrameTime
+let yVelocity
 
 export function setUpCharacter () { 
-   isJumping = false    
+   isJumping = true  
    characterFrame = 0
    currentFrameTime = 0
+    yVelocity = 0
+     setCustomProperty(characterElement,"--bottom",0)
+   document.removeEventListener("keydown",jump)
+    document.addEventListener("keydown",jump)
 }
 
 export function updateCharacter(deltaTime,speedScale){
    handleRun(deltaTime,speedScale)
-    handleJump()
+    handleJump(deltaTime)
 }
 
 function handleRun(deltaTime,speedScale){
     if(isJumping) {
-        characterElement = `url(./game_images/characters/homer_sprite_jumping.png)`
+        characterElement.src = `game_images/characters/homer_sprite_jumping.png`
         return
     }
 
@@ -35,7 +42,26 @@ function handleRun(deltaTime,speedScale){
 
     currentFrameTime += deltaTime * speedScale}
 
-function handleJump(){
-    const top = parseFloat(characterElement.style.getPropertyValue("--top"))
-   
+function handleJump(deltaTime){
+    if(!isJumping) return
+
+    incrementCustomProperty(characterElement,"--bottom", yVelocity * deltaTime)
+    
+    if(getCustomProperty(characterElement,"--bottom") <= 0){
+        isJumping = false
+        setCustomProperty(characterElement,"--bottom",0)
+    }
+    yVelocity -= GRAVITY * deltaTime
+
+
 }
+
+function jump(e){ 
+    if(e.code !== "Space" || isJumping) return
+    yVelocity = JUMPSPEED
+    isJumping = true
+
+}
+
+
+
